24c306f2fd2f4627a93d75178f63adce0e8fba35
[Yulia Lebedeva] <yl.75@mail.ru>
2023-03-18 13:45:19 +0500

    add task 00

---
+++ seminar04/Task00.java
@@ -0,0 +1,31 @@
+package seminar04;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Task00 {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        Random random = new Random();
+        long startTimeArray = System.nanoTime();
+
+        for (int i = 0; i < 1000000; i++)
+            arrayList.add(random.nextInt());
+        Long endTimeArray = System.nanoTime();
+
+        List<Integer> linkedList = new LinkedList<>();
+        long startTimeLinked = System.nanoTime();
+
+        for (int i = 0; i < 1000000; i++)
+            linkedList.add(random.nextInt());
+        long endTimeLinked = System.nanoTime();
+
+        System.out.println("Время заполнения ArrayList  = " + (endTimeArray - startTimeArray));
+        System.out.println("Время заполнения LinkedList = " + (endTimeLinked - startTimeLinked));
+    }
+}
+
+// Время заполнения ArrayList  = 137118864
+// Время заполнения LinkedList = 156094410
